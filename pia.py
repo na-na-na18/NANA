b37b23de82f258df4ff0e9c26b6f600178e698c3
Rheena Mary Piamonte <piamonte.rheenamary@gmail.com>
2026-02-03 00:11:10 +0800

    week 2

---
+++ pia.py
@@ -0,0 +1,16 @@
+fullname = "Rheena Mary Salva Piamonte"
+age = 18
+height = 1.53
+is_working = False
+
+print("Fullname:", fullname)
+print(type(fullname))
+
+print("\nAge:", age)
+print(type(age))
+
+print("\nHeight in meters:", height)
+print(type(height))
+
+print("\nCurrently working?", is_working)
+print(type(is_working))
